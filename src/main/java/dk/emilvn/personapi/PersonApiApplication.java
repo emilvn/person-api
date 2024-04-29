33379b9ea69c977d636365f1394681cdf20ce9b5
emilvn <emilvnielsen@hotmail.com>
2024-04-29 16:47:26 +0200

    rename packages

--- src/main/java/dk/emilvn/personapi/PersonApiApplication.java
+++ src/main/java/dk/emilvn/personapi/PersonApiApplication.java
@@ -1,4 +1,4 @@
-package com.example.personapi;
+package dk.emilvn.personapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
